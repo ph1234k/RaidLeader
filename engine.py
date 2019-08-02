b8a8d2e27660c4acad82ff3a8dc48bdb9769f1af
Stephanie Rice <ph1234k@hotmail.com>
2019-08-01 22:03:38 -0500

    Initial commit
    Contains 'working' version in which @ can walk around.
    
    Contains initial render functions for characters in color.
    
    Contains base Entity class
    
    Contains base input handlers for moving, fullscreen switching and exiting.

---
+++ engine.py
@@ -0,0 +1,47 @@
+import tcod as libtcod
+from entity import Entity
+from input_handlers import handle_keys
+from render_functions import clear_all, render_all
+
+def main():
+    screen_width = 80
+    screen_height = 50
+
+    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', libtcod.yellow)
+    entities = [npc, player]
+
+    libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+
+    libtcod.console_init_root(screen_width, screen_height, 'NewRL', False)
+
+    con = libtcod.console_new(screen_width, screen_height)
+    key = libtcod.Key()
+    mouse = libtcod.Mouse()
+
+    while not libtcod.console_is_window_closed():
+        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
+       
+        render_all(con, entities, screen_width, screen_height)
+        libtcod.console_flush()
+
+        clear_all(con, entities)
+
+        action = handle_keys(key)
+
+        move = action.get('move')
+        exit = action.get('exit')
+        fullscreen = action.get('fullscreen')
+
+        if move:
+        	dx, dy = move
+        	player.move(dx, dy)
+
+        if exit:
+            return True
+
+        if fullscreen:
+        	libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+
+if __name__ == '__main__':
+    main()
