5c0337eea0835e29f2fe6397995c937f20699ecf
Stephanie Rice <ph1234k@hotmail.com>
2019-08-02 12:06:43 -0500

    Added a light README
    Added in basic map, tiles, and updated render functions for map drawing
    
    Added in basic wall physics for players, can be easily extended to other entities

---
+++ README.md
@@ -0,0 +1,7 @@
+# Skeleton for some RL games
+
+## Goals
+
+1. A playable engine
+2. Simple to add, remove and adjust entities
+3. Simple to adjust to novel mechanics
--- render_functions.py
+++ render_functions.py
@@ -1,6 +1,16 @@
 import tcod as libtcod
 
-def render_all(con, entities, screen_width, screen_height):
+def render_all(con, entities, game_map, screen_width, screen_height, colors):
+	# Draw all tiles in game_map
+	for y in range(game_map.height):
+		for x in range(game_map.width):
+			wall = game_map.tiles[x][y].block_sight
+
+			if wall:
+				libtcod.console_set_char_background(con, x, y, colors.get('dark_wall'), libtcod.BKGND_SET)
+			else:
+				libtcod.console_set_char_background(con, x, y, colors.get('dark_ground'), libtcod.BKGND_SET)
+
 	# Draw all entities in the list
 	for entity in entities:
 		draw_entity(con, entity)
